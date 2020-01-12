9e8a89a2ac3c2a1008623d3d6c3a3c8a972cf5b1
CuiMingyu <mingyuc36@163.com>
2020-01-12 16:16:13 +0800

    1.Num Sum

---
+++ 1.cpp
@@ -0,0 +1,25 @@
+//
+//  1.cpp
+//  
+//
+//  Created by Cui on 2020/1/12.
+//
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int index = nums.size();
+        vector<int> result;
+
+        for (int i = 0; i < index - 1; ++i){
+            for (int j = i + 1; j < index; ++j){
+                if (nums[i] + nums[j] == target){
+                    result.push_back(i);
+                    result.push_back(j);
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+};
